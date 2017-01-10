016d2e0c47a8b35bca20a32b64fb4e1d84d05793
JelleKloosterboer <jelle.kloosterboer97@gmail.com>
2017-01-10 22:03:27 +0100

    Start
    BEGIN NOW

---
+++ intake.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<!-- Add a CSS below
+<link rel="stylesheet" type="text/css" href="__.css"> -->
+<!-- Enter a Javascript here
+<script type="text/javascript" src="__.js"></script> -->
+<html>
+<title>
+<!-- this is your title -->
+</title>
+<head>
+<!-- this is your head -->
+</head>
+<body>
+<!-- this is your body -->
+<?php
+	echo "Hello";
+?>
+</body>
+</html>
