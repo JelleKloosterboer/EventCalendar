1c16cd2052b59de84a62180a565ddac577d323e6
JelleKloosterboer <jelle.kloosterboer97@gmail.com>
2017-01-13 15:51:07 +0100

    created a dashboard file
    TODO: Make datetime only display dates and in correct format

---
+++ dashboard.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<!-- Add a CSS below
+<link rel="stylesheet" type="text/css" href="__.css"> -->
+<!-- Enter a Javascript here
+<script type="text/javascript" src="__.js"></script> -->
+<?php
+	// connect to the database + select database
+	include_once "mysql_connect.php";
+	//select ALL data in the table
+	$sql = "SELECT * FROM evenement";
+	// store above command in the variable $records
+	$records = mysql_query($sql);
+?>
+<html>
+<title>
+Dashbord
+</title>
+<head>
+
+</head>
+<body>
+<!-- simple HTML table with a line for every field in the table -->
+<table border="1" cellpadding = "1" cellspacing = "1">
+  <tr>
+    <th>Evenementnaam</th>
+	<th>Startdatum</th>
+	<th>Einddatum</th>
+	<th>Datum voorbereiding</th>
+	<th>Soort evenement</th>
+	<th>Opmerking</th>
+	<th>Briefing Map</th>
+	<th>Afgerond?</th>
+  <tr>
+  
+  <?php
+  // Make a new row in the HTML table for every record in the database
+	while($event=mysql_fetch_assoc($records)) {
+		echo "<tr>";
+		echo "<td>".$event['naam']."</td>";
+		echo "<td>".$event['start_datum']."</td>";
+		echo "<td>".$event['eind_datum']."</td>";
+		echo "<td>".$event['voorbereiding_datum']."</td>";
+		echo "<td>".$event['soort_evenement']."</td>";
+		echo "<td>".$event['opmerking']."</td>";
+		echo "<td>".$event['briefing_map']."</td>";
+		echo "<td>".$event['afgerond']."</td>";
+		echo "<tr/>";
+	}
+  ?>
+</table>
+</body>
+</html>
